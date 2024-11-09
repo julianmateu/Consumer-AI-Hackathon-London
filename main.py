1d3cfd00e58702b243e9ce7eceec4105cff58199
Matt <matthewrharding@icloud.com>
2024-11-09 12:54:33 +0000

    Added elevenlabs lib

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import os
 from utils import logger
 from dotenv import load_dotenv
 
+from elevenlabs import play
+from elevenlabs.client import ElevenLabs
+
+
 load_dotenv()
 
 ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
 
+elevenlabs_client = ElevenLabs(
+  api_key=ELEVENLABS_API_KEY
+)
+
 def main():
-    logger.info("ELEVEN_LABS_API_KEY: " + ELEVENLABS_API_KEY)
+    response = elevenlabs_client.voices.get_all()
+    # audio = elevenlabs_client.generate(text="Hello there!", voice=response.voices[0])
+    logger.info(response.voices)
 
 if __name__ == "__main__":
     main()
