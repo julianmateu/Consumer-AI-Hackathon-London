0bff4dc6832af0b25e090e65e169c10944501265
Matt <matthewrharding@icloud.com>
2024-11-09 12:17:17 +0000

    Set up logging

---
+++ main.py
@@ -0,0 +1,9 @@
+from utils import logger
+
+def main():
+    logger.info("Starting the main process.")
+    # Your main code logic here
+    logger.info("Finished the main process.")
+
+if __name__ == "__main__":
+    main()
