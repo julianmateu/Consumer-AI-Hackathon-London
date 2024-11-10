8776611f6b5159bc9afd1597bda728c28b0c3a1f
Matt <matthewrharding@icloud.com>
2024-11-10 13:00:27 +0000

    Patch images from metadata

--- frontend/app/picture_upload/PhotoUploadPage.tsx
+++ frontend/app/picture_upload/PhotoUploadPage.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const PhotoUploadPage = () => {
   const [loading, setLoading] = useState(false);
-  const [file, setFile] = useState<File|null>(null);
+  const [file, setFile] = useState<File | null>(null);
   const params = useSearchParams();
 
   // Handle file selection
@@ -13,28 +13,28 @@ const PhotoUploadPage = () => {
     // e = e as unknown as typeof e & { target: {files: []} };
     if (!e.target.files) {
       setFile(null);
-    } else if  (e.target.files.length > 0) {
+    } else if (e.target.files.length > 0) {
       setFile(e.target.files[0]);
     }
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
+
     if (!file) {
       alert("Please select an image file");
       return;
     }
-  
+
     // Read file and convert to base64
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    
+
     reader.onloadend = async () => {
       setLoading(true); // Start loading
-      
+
       const base64Image = (reader.result as string)?.split(",")[1]; // Remove data URL prefix
-  
+
       try {
         const response = await fetch("/api/upload-image", {
           method: "POST",
@@ -43,9 +43,9 @@ const PhotoUploadPage = () => {
           },
           body: JSON.stringify({ image: base64Image }),
         });
-  
+
         const data = await response.json();
-  
+
         if (response.ok) {
           // Persist data using @vercel/sql
           const response2 = await fetch("/api/save-image-result", {
@@ -55,17 +55,17 @@ const PhotoUploadPage = () => {
             },
             body: JSON.stringify(data),
           });
-  
+
           const data2 = await response2.json();
-  
+
           if (!response2.ok) {
             console.error(data2.error);
             alert("Error: " + data2.error);
             return;
           }
-  
+
           const nextPage = `/report?vehicle=${params.get("vehicle")}`;
-  
+
           // redirect to the next page
           window.location.href = nextPage;
         } else {
@@ -98,9 +98,10 @@ const PhotoUploadPage = () => {
             <div className="spinner"></div>
           </div>
         )}
-        
+
         {!loading && (
           <form onSubmit={handleSubmit}>
+            <label>Please upload a picture of the damage to your vehicle</label>
             <input
               type="file"
               accept="image/*"
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['upload.wikimedia.org','encrypted-tbn0.gstatic.com'], // Add the domain here
+  },
 };
 
 export default nextConfig;
